74c9594b1f7fe7f392c77cbdfbddf36d68e1fa4b
mysoyul <vega2k_@naver.com>
2023-09-26 12:31:56 +0900

    vuejs2 todoapp

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" ref="todoItem" @keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    //LifeCycle Hook method
+    //$refs는 document.getElementById(id) 함수처럼 html dom에 직접 접근할 때 사용되는 객체
+    //ref="todoItem"의 ref 속성은 기존의 id 속성과 동일한 속성이다
+    mounted() {
+        this.$refs.todoItem.focus();
+    },
+    data() {
+        return {
+            newTodoItem: ""
+        }
+    }, //data
+    methods: {
+        addTodo() {
+            if (this.newTodoItem !== '') {
+                localStorage.setItem(this.newTodoItem, this.newTodoItem);
+                this.clearInput();
+            }
+        }, //addTodo
+        clearInput() {
+            this.newTodoItem = '';
+        }, //clearTodo
+
+
+    }, //methods
+}
+</script>
+
+<style scoped>
+input:focus {
+    outline: none;
+}
+
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        List
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todoItems: []
+        }
+    },
+    /* life cycle method */
+    created() {
+        if (localStorage.length > 0) {
+            for (var i = 0; i < localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    }
+}
+</script>
+
+<style></style>
