51f7b3c08f1506dd27bb19e11693f37875b57c4a
mysoyul <vega2k_@naver.com>
2023-09-27 10:09:16 +0900

    store add state object add

---
+++ src/store/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const storage = {
+    fetch() {
+        const arr = [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            }
+        }
+        return arr;
+    },
+};
+
+export const store = new Vuex.Store({
+    //state, actions, mutations, getters, modules
+    state: {
+        todoItems: storage.fetch()
+    },
+
+});
